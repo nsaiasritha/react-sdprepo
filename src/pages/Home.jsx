e45e6a37ae932021db7b90c175bc19f05a3abdd4
aasritha <2500030032@kluniversity.in>
2026-02-21 23:05:05 +0530

    modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home-container">
+      <h1 className="home-title">Home Page</h1>
     </div>
   );
 };
